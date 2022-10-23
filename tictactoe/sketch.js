0deff3012e29bd58cad4904da0379ea7108ef312
PlagueyPlatypus <114016382+PlagueyPlatypus@users.noreply.github.com>
2022-10-23 12:22:05 -0600

    Update sketch.js

--- tictactoe/sketch.js
+++ tictactoe/sketch.js
@@ -5,9 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const playerX = "x"
-const playerO = "o"
-const wins = [
+const PLAYER_X_CLASS = 'x'
+const PLAYER_O_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -18,41 +18,119 @@ const wins = [
 	[2, 4, 6]
 ]
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+const cellElements = document.querySelectorAll('[data-cell]')
+const boardElement = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.getElementById('winningMessageText')
+let isPlayer_O_Turn = false
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+	isPlayer_O_Turn = false
+	cellElements.forEach(cell => {
+		cell.classList.remove(PLAYER_X_CLASS)
+		cell.classList.remove(PLAYER_O_CLASS)
+		cell.removeEventListener('click', handleCellClick)
+		cell.addEventListener('click', handleCellClick, { once: true })
+	})
+	setBoardHoverClass()
+	winningMessageElement.classList.remove('show')
 }
 
-function windowResized() {
-  setup();
+function handleCellClick(e) {
+	const cell = e.target
+	const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS
+	placeMark(cell, currentClass)
+	if (checkWin(currentClass)) {
+		endGame(false)
+	} else if (isDraw()) {
+		endGame(true)
+	} else {
+		swapTurns()
+		setBoardHoverClass()
+	}
 }
 
-function draw() {
-  background("white");
-  drawGraph();
-  if (state === "x") {
-    drawCounter();
-  }
+// function endGame(draw) {
+//   if (draw) {
+//     winningMessageTextElement.innerText = "It's a draw!"
+//   }
+//   else {
+//     winningMessageTextElement.innerText = 'Player with ${isPlayer_O_turn ? "O's" : "X's"} wins!'
+//   }
+// }
+
+function isDraw() {
+	return [...cellElements].every(cell => {
+		return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
+	})
 }
 
-function drawGraph() {
-  let cellWidth = width/3;
-  let cellHeight = height/3;
-  if (cellWidth > cellHeight) {
-    cellWidth = cellHeight;
-  }
-  else {
-    cellHeight = cellWidth;
-  }
-
-  let isWhite = true;
-  for (let y = 0; y < 3; y++) {
-    for (let x = 0; x < 3; x++) {
-      if (isWhite) {
-        fill("white");
-      }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      isWhite = !isWhite;
-    }
-    isWhite = !isWhite;
-  }
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass)
 }
+
+function swapTurns() {
+	isPlayer_O_Turn = !isPlayer_O_Turn
+}
+
+function setBoardHoverClass() {
+	boardElement.classList.remove(PLAYER_X_CLASS)
+	boardElement.classList.remove(PLAYER_O_CLASS)
+	if (isPlayer_O_Turn) {
+		boardElement.classList.add(PLAYER_O_CLASS)
+	} else {
+		boardElement.classList.add(PLAYER_X_CLASS)
+	}
+}
+
+function checkWin(currentClass) {
+	return WINNING_COMBINATIONS.some(combination => {
+		return combination.every(index => {
+			return cellElements[index].classList.contains(currentClass)
+		})
+	})
+}
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+// function windowResized() {
+//   setup();
+// }
+
+// function draw() {
+//   background("white");
+//   drawGraph();
+//   if (state === "x") {
+//     drawCounter();
+//   }
+// }
+
+// function drawGraph() {
+//   let cellWidth = width/3;
+//   let cellHeight = height/3;
+//   if (cellWidth > cellHeight) {
+//     cellWidth = cellHeight;
+//   }
+//   else {
+//     cellHeight = cellWidth;
+//   }
+
+//   let isWhite = true;
+//   for (let y = 0; y < 3; y++) {
+//     for (let x = 0; x < 3; x++) {
+//       if (isWhite) {
+//         fill("white");
+//       }
+//       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+//       isWhite = !isWhite;
+//     }
+//     isWhite = !isWhite;
+//   }
+// }
